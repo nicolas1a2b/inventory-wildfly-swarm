543f164b4f86fd67e00b8ba0c715ea3dad2432cb
Developer <developer@me.com>
2018-11-07 15:32:32 +0100

    initial add

---
+++ src/main/java/com/redhat/cloudnative/inventory/InventoryApplication.java
@@ -0,0 +1,9 @@
+package com.redhat.cloudnative.inventory;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class InventoryApplication extends Application {
+}
+
